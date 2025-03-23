37669f8ab9cf1647778c5c1d40ca671b40e2f640
PoojithaPola <137188518+PoojithaPola@users.noreply.github.com>
2025-03-23 16:30:13 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to Food Delivery app</h1>
+      <img src="./Grabnow_hpage.png" alt="" 
+      style={{width:"1300px",height:"600px"}}/>
+
+    </div>
+  )
+}
+
+export default Home
